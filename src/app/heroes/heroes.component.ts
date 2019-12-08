82eddd41febedadd127a163325570c059b9276e7
nnthanh1996 <nnthanh1996@gmail.com>
2019-12-08 12:27:01 +0700

    3 files modified, 3 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HEROES } from '../../assets/heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
