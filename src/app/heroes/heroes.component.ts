c000c38cccc13115e9d72a929aa3e94d742efb56
nnthanh1996 <nnthanh1996@gmail.com>
2019-12-08 12:45:12 +0700

    2 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../../assets/heroes';
+import {Hero} from '../../assets/hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +8,14 @@ import { HEROES } from '../../assets/heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero : Hero;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
