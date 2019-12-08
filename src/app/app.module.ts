f1ec6ac8afed2558cffc027db405c29cd8634c8c
nnthanh1996 <nnthanh1996@gmail.com>
2019-12-08 17:20:02 +0700

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroesDetailsComponent } from './heroes-details/heroes-details.component';
+import { HeroesService } from './heroes.service';
+import { HeroesMessagesComponent } from './heroes-messages/heroes-messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroesDetailsComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeroesComponent, HeroesDetailsComponent, HeroesMessagesComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [HeroesService, MessageService]
 })
 export class AppModule { }
---
+++ src/app/heroes-messages/heroes-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-messages',
+  templateUrl: './heroes-messages.component.html',
+  styleUrls: ['./heroes-messages.component.css']
+})
+export class HeroesMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
