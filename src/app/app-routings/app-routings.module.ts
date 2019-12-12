f2bc73b783f26a9aad76e6fbb45c5f1d57bcab64
nnthanh1996 <nnthanh1996@gmail.com>
2019-12-12 23:13:18 +0700

    4 files modified

--- src/app/app-routings/app-routings.module.ts
+++ src/app/app-routings/app-routings.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from '../heroes/heroes.component';
 import { DashboardComponent } from "../dashboard/dashboard.component";
-
+import { HeroesDetailsComponent } from "../heroes-details/heroes-details.component";
 const routes : Routes = [
   {path: 'heroes', component: HeroesComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component: HeroesDetailsComponent}
 ]
 
 @NgModule({
--- src/app/heroes-details/heroes-details.component.ts
+++ src/app/heroes-details/heroes-details.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Hero} from '../../assets/hero';
-
+import { HeroesService } from "../heroes.service";
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-heroes-details',
   templateUrl: './heroes-details.component.html',
   styleUrls: ['./heroes-details.component.css']
 })
 export class HeroesDetailsComponent implements OnInit {
-  @Input() hero : Hero;
-  constructor() { }
+  hero : Hero;
+  id: string;
+  constructor(private heroService: HeroesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+  }
+
+  getHeroes(): void {
+    let params = this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+    })
+    this.heroService.getHeroes().subscribe(heroes => this.hero = heroes[this.id]);
   }
 
 }
