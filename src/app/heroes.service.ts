e0d961acd92916ca8ae216ef08d8c122ec7a97a7
nnthanh1996 <nnthanh1996@gmail.com>
2019-12-14 13:15:38 +0700

    2 files modified

--- src/app/heroes.service.ts
+++ src/app/heroes.service.ts
@@ -3,16 +3,21 @@ import { MessageService } from './message.service';
 import { HEROES } from '../assets/heroes';
 import { Hero } from '../assets/hero';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroesService {
 
-  constructor(public messageService : MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(public messageService : MessageService, private http : HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('Hero Service: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
 
-    return of(HEROES);
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
